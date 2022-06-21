95b2d24c6f5fbba315aa2b4af92f2160f808a5a6
Heeramani Madhwani <78553096+HeeramaniMadhwani@users.noreply.github.com>
2022-06-21 17:44:41 +0530

    Delete BetterReads_UserService directory

--- BetterReads_UserService/src/main/java/com/capgemini/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.capgemini.model.User;
-
-public interface UserRepository extends MongoRepository<User, Integer> {
-
-}
